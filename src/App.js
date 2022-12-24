6a0cd8173b7bbe5e57aaed3260932fa10cfed231
frarm <frarm2014@outlook.com>
2022-12-23 22:28:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      My Todo Application
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
     </div>
   );
+}
+
+//function component
+function FirstComponent(){
+  return (
+    <div className='FirstComponent'>First Component</div>
+  );
+}
+
+//class component
+class SecondComponent extends Component{
+  render(){
+    return (
+      <div className='SecondComponent'>Second Component</div>
+    )
+  }
 }
 
 export default App;
