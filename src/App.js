8d61252c83f2c8d5e3b03ba82c10d9b4a4dfd00e
frarm <frarm2014@outlook.com>
2022-12-24 21:44:12 -0500

    Section11

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import './App.css';
-import Counter from './components/counter/Counter';
+import TodoApp from './components/todo/TodoApp';
 
 function App() {
   return (
     <div className="App">
-      {/* <PlayingWithProps property1="value1" property2="value2"></PlayingWithProps> */}
-      <Counter></Counter>
+      <TodoApp></TodoApp>
     </div>
   );
 }
-
-// function PlayingWithProps(properties){
-//   console.log(properties.property1);
-//   return (
-//     <div>Props</div>
-//   )
-// }
-
-// function PlayingWithProps({property1, property2}){
-//   console.log(property1);
-//   console.log(property2);
-//   return (
-//     <div>Props</div>
-//   )
-// }
-
 export default App;
